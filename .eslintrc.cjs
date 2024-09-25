655238de356de27cb4ac1f600ac3332d0cc04bae
DooNas <jaejoon4929@gmail.com>
2024-09-26 03:45:37 +0900

    api 데이터 파싱

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -16,6 +16,7 @@ module.exports = {
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
+    'no-unused-vars': 'warn',  // 경고를 단순 알림으로 변경
     'react/jsx-no-target-blank': 'off',
     'react-refresh/only-export-components': [
       'warn',
